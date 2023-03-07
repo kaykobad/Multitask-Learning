ebbdd0e5f510cc88bf7b16e822ee01253d1e68d2
MDKAYKOBAD REZA <mreza@crisgc.engr.ucr.edu>
2023-03-07 15:53:56 -0800

    Update code

--- Object-Detection/simple_baselines.py
+++ Object-Detection/simple_baselines.py
@@ -19,14 +19,16 @@ print("Device:", device, torch.cuda.device_count())
 
 # Global Variables
 should_init_wandb = True
-enable_wandb = True
+enable_wandb_logging = True
 wandb_name = "EO-R50"
-batch_size = 64
-data_folder = "C:\\Kaykobad\\Multi-Modal-Datasets\\Aerial-View-Obj-ClassificationPBVS22\\train_processed\\"
-EO_data_folder = data_folder + "EO\\"
-SAR_data_folder = data_folder + "SAR\\"
+model_name = "R50-EO"
+batch_size = 512
+data_folder = "dataset/train_processed/"
+EO_data_folder = data_folder + "EO/"
+SAR_data_folder = data_folder + "SAR/"
 random_state = 44
 random_seed = 0
+num_epoches = 30
 
 # wandb init
 if should_init_wandb:
@@ -34,70 +36,70 @@ if should_init_wandb:
 
 
 def log_wandb(wandb_data):
-    if enable_wandb:
+    if should_init_wandb and enable_wandb_logging:
         wandb.log(wandb_data)
 
 
-def parse_args():
-    """
-    Parse input arguments
-    Returns
-    -------
-    args : object
-        Parsed args
-    """
-    h = {
-        "program": "Simple Baselines training",
-        "train_folder": "Path to training data folder.",
-        "batch_size": "Number of images to load per batch. Set according to your PC GPU memory available. If you get "
-                      "out-of-memory errors, lower the value. defaults to 64",
-        "epochs": "How many epochs to train for. Once every training image has been shown to the CNN once, an epoch "
-                  "has passed. Defaults to 15",
-        "test_folder": "Path to test data folder",
-        "num_workers": "Number of workers to load in batches of data. Change according to GPU usage",
-        "test_only": "Set to true if you want to test a loaded model. Make sure to pass in model path",
-        "model_path": "Path to your model",
-        "learning_rate": "The learning rate of your model. Tune it if it's overfitting or not learning enough"}
-    parser = argparse.ArgumentParser(description=h['program'], formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--train_folder', help=h["train_folder"], type=str)
-    parser.add_argument('--batch_size', help=h['batch_size'], type=int, default=64)
-    parser.add_argument('--epochs', help=h["epochs"], type=int, default=15)
-    parser.add_argument('--test_folder', help=h["test_folder"], type=str)
-    parser.add_argument('--num_workers', help=h["num_workers"], type=int, default=5)
-    parser.add_argument('--test_only', help=h["test_only"], type=bool, default=False)
-    parser.add_argument('--model_path', help=h["num_workers"], type=str),
-    parser.add_argument('--learning_rate', help=h["learning_rate"], type=float, default=0.003)
+# def parse_args():
+#     """
+#     Parse input arguments
+#     Returns
+#     -------
+#     args : object
+#         Parsed args
+#     """
+#     h = {
+#         "program": "Simple Baselines training",
+#         "train_folder": "Path to training data folder.",
+#         "batch_size": "Number of images to load per batch. Set according to your PC GPU memory available. If you get "
+#                       "out-of-memory errors, lower the value. defaults to 64",
+#         "epochs": "How many epochs to train for. Once every training image has been shown to the CNN once, an epoch "
+#                   "has passed. Defaults to 15",
+#         "test_folder": "Path to test data folder",
+#         "num_workers": "Number of workers to load in batches of data. Change according to GPU usage",
+#         "test_only": "Set to true if you want to test a loaded model. Make sure to pass in model path",
+#         "model_path": "Path to your model",
+#         "learning_rate": "The learning rate of your model. Tune it if it's overfitting or not learning enough"}
+#     parser = argparse.ArgumentParser(description=h['program'], formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+#     parser.add_argument('--train_folder', help=h["train_folder"], type=str)
+#     parser.add_argument('--batch_size', help=h['batch_size'], type=int, default=64)
+#     parser.add_argument('--epochs', help=h["epochs"], type=int, default=15)
+#     parser.add_argument('--test_folder', help=h["test_folder"], type=str)
+#     parser.add_argument('--num_workers', help=h["num_workers"], type=int, default=5)
+#     parser.add_argument('--test_only', help=h["test_only"], type=bool, default=False)
+#     parser.add_argument('--model_path', help=h["num_workers"], type=str),
+#     parser.add_argument('--learning_rate', help=h["learning_rate"], type=float, default=0.003)
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    return args
-
-
-def load_train_data(train_data_path, batch_size):
-    # Convert images to tensors, normalize, and resize them
-    transform = transforms.Compose([
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
-
-    train_data = torchvision.datasets.ImageFolder(root=train_data_path, transform=transform)
-    train_data_loader = data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=args.num_workers)
-
-    return train_data_loader
+#     return args
 
 
-def load_test_data(test_data_path, batch_size):
-    transform = transforms.Compose([
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
+# def load_train_data(train_data_path, batch_size):
+#     # Convert images to tensors, normalize, and resize them
+#     transform = transforms.Compose([
+#         transforms.Resize(224),
+#         transforms.ToTensor(),
+#         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+#     ])
 
-    test_data = torchvision.datasets.ImageFolder(root=test_data_path, transform=transform)
-    test_data_loader = data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=args.num_workers)
+#     train_data = torchvision.datasets.ImageFolder(root=train_data_path, transform=transform)
+#     train_data_loader = data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=args.num_workers)
 
-    return test_data_loader
+#     return train_data_loader
+
+
+# def load_test_data(test_data_path, batch_size):
+#     transform = transforms.Compose([
+#         transforms.Resize(224),
+#         transforms.ToTensor(),
+#         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+#     ])
+
+#     test_data = torchvision.datasets.ImageFolder(root=test_data_path, transform=transform)
+#     test_data_loader = data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=args.num_workers)
+
+#     return test_data_loader
 
 
 def load_data(data_path):
@@ -120,35 +122,68 @@ def load_data(data_path):
     # Subset dataset for train and val
     train_dataset = data.Subset(dataset, train_idx)
     validation_dataset = data.Subset(dataset, valid_idx)
-    print(len(dataset), len(train_dataset), len(validation_dataset))
 
     # test_data_loader = data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=args.num_workers)
     # Dataloader for train and val
     train_loader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     validation_loader = data.DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
 
+    # Print info
+    print(f"Total number of train samples: {len(train_dataset)}")
+    print(f"Total number of validation samples: {len(validation_dataset)}")
+    print(f"Total number of train batches: {len(train_loader)}")
+    print(f"Total number of validation batches: {len(validation_loader)}")
+    print()
+    # print(len(dataset), len(train_dataset), len(validation_dataset))
+
     return train_loader, validation_loader
 
 
-def train():
-    args = parse_args()
-    train_data = load_train_data(args.train_folder, args.batch_size)
-    train_losses = []
+# My evaluation function
+def eval_model(model, validation_data):
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        with tqdm(validation_data, unit="batch", desc="Eval") as tepoch:
+            for images, labels in tepoch:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                predictions = outputs.argmax(dim=1, keepdim=True).squeeze()
+                correct += (predictions == labels).sum().item()
+                total += labels.size(0)
 
-    device = torch.device("cuda" if torch.cuda.is_available()
-                          else "cpu")
+    accuracy = 1.0 * correct / total
+    print('Validation Accuracy on the ' + str(total) + ' validation images: ' + str(round(accuracy, 2)))
+    return accuracy
+
+
+# My training function
+def train_model(data_path):
+    train_data, validation_data = load_data(data_path)
+    # train_losses = []
+
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = models.resnet18(pretrained=False)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+    model = nn.DataParallel(model)
     model.to(device)
 
-    for epoch in range(args.epochs):  # loop over the dataset multiple times
-        with tqdm(train_data, unit="batch") as tepoch:
-            for inputs, labels in tepoch:
+    # Save the best model
+    best_model = None
+    best_accuracy = 0.0
 
-                tepoch.set_description(f"Epoch {epoch}")
+    # loop over the dataset multiple times
+    for epoch in range(num_epoches):  
+        correct = 0
+        total = 0
+        train_loss = 0
+        with tqdm(train_data, unit="batch", desc="Train Epoch {epoch}") as tepoch:
+            for inputs, labels in tepoch:
+                # tepoch.set_description(f"Epoch {epoch}")
                 # get the inputs
                 inputs, labels = inputs.to(device), labels.to(device)
 
@@ -158,44 +193,106 @@ def train():
                 # forward + get predictions + backward + optimize
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
+                train_loss += loss.item()
 
                 predictions = outputs.argmax(dim=1, keepdim=True).squeeze()
-                correct = (predictions == labels).sum().item()
-                accuracy = correct / args.batch_size
+                correct_prediction = (predictions == labels).sum().item()
+                correct += correct_prediction
+                total += labels.size(0)
 
                 loss.backward()
                 optimizer.step()
 
-                tepoch.set_postfix(loss=loss.item(), accuracy=100. * accuracy)
+                tepoch.set_postfix(loss=loss.item(), accuracy=100. * correct_prediction/labels.size(0))
+        
+        train_accuracy = 1.0 * correct / total
+        validation_accuracy = eval_model(model, validation_data)
+
+        # Save best model
+        if validation_accuracy > best_accuracy:
+            best_accuracy = validation_accuracy
+            best_model = model
+
+        log_data = {
+            "Epoch": epoch,
+            "Train Loss": train_loss,
+            "Train Accuracy": train_accuracy,
+            "Train Error": 1.0 - train_accuracy,
+            "Validation Accuracy": validation_accuracy,
+            "Validation Error": 1.0 - validation_accuracy,
+        }
+        log_wandb(log_data)
+
+    print('Finished Training with best validation accuracy {best_accuracy}')
+    torch.save(best_model, 'check_points/{model_name}.pth')
 
 
-    print('Finished Training')
-    torch.save(model, 'unicornmodel.pth')
+# def train():
+#     args = parse_args()
+#     train_data = load_train_data(args.train_folder, args.batch_size)
+#     train_losses = []
+
+#     device = torch.device("cuda" if torch.cuda.is_available()
+#                           else "cpu")
+
+#     model = models.resnet18(pretrained=False)
+
+#     criterion = nn.CrossEntropyLoss()
+#     optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
+#     model.to(device)
+
+#     for epoch in range(args.epochs):  # loop over the dataset multiple times
+#         with tqdm(train_data, unit="batch") as tepoch:
+#             for inputs, labels in tepoch:
+
+#                 tepoch.set_description(f"Epoch {epoch}")
+#                 # get the inputs
+#                 inputs, labels = inputs.to(device), labels.to(device)
+
+#                 # zero the parameter gradients
+#                 optimizer.zero_grad()
+
+#                 # forward + get predictions + backward + optimize
+#                 outputs = model(inputs)
+#                 loss = criterion(outputs, labels)
+
+#                 predictions = outputs.argmax(dim=1, keepdim=True).squeeze()
+#                 correct = (predictions == labels).sum().item()
+#                 accuracy = correct / args.batch_size
+
+#                 loss.backward()
+#                 optimizer.step()
+
+#                 tepoch.set_postfix(loss=loss.item(), accuracy=100. * accuracy)
 
 
-def test(model_path):
-    args = parse_args()
-    test_data = load_test_data(args.test_folder, args.batch_size)
+#     print('Finished Training')
+#     torch.save(model, 'unicornmodel.pth')
 
-    device = torch.device("cuda" if torch.cuda.is_available()
-                          else "cpu")
 
-    model = torch.load(model_path)
-    model.to(device)
+# def test(model_path):
+#     args = parse_args()
+#     test_data = load_test_data(args.test_folder, args.batch_size)
 
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in test_data:
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+#     device = torch.device("cuda" if torch.cuda.is_available()
+#                           else "cpu")
 
-    print('Accuracy of the network on the ' + str(total) + ' test images: %d %%' % (
-            100 * correct / total))
+#     model = torch.load(model_path)
+#     model.to(device)
+
+#     correct = 0
+#     total = 0
+#     with torch.no_grad():
+#         for data in test_data:
+#             images, labels = data
+#             images, labels = images.to(device), labels.to(device)
+#             outputs = model(images)
+#             _, predicted = torch.max(outputs.data, 1)
+#             total += labels.size(0)
+#             correct += (predicted == labels).sum().item()
+
+#     print('Accuracy of the network on the ' + str(total) + ' test images: %d %%' % (
+#             100 * correct / total))
 
 
 if __name__ == "__main__":
@@ -204,4 +301,5 @@ if __name__ == "__main__":
     #     test(args.model_path)
     # else:
     #     train()
-    load_data(EO_data_folder)
+    # load_data(EO_data_folder)
+    train_model(EO_data_folder)
